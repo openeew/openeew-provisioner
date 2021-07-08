fba4a9273d44a8978131b424b687eb81fff6e768
John Walicki <johnwalicki@gmail.com>
2021-07-07 20:51:35 -0400

    Remove Carbon prototype
    Signed-off-by: John Walicki <johnwalicki@gmail.com>

--- lib/theme/carbon.dart
+++
@@ -1,6 +0,0 @@
-library carbon;
-
-export './models/models.index.dart';
-export './utils/utils.index.dart';
-export './widgets/widgets.index.dart';
-export './styles/styles.index.dart';
--- lib/theme/utils/extensions/widgets.extinsion.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:openeew_provisioner/theme/carbon.dart';
-
-extension CFormContextExtension on BuildContext {
-  CFormState get cform {
-    return CForm.of(this);
-  }
-}
